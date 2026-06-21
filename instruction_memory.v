eea1d3425ca5c5201009666ea7bd1fb129f07d73
gatlaanirudh2006 <gatlaanirudh2006@gmail.com>
2026-06-22 00:03:05 +0530

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2026 11:37:54
-// Design Name: 
-// Module Name: instruction_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module instruction_memory (
     input [31:0] address,
     output [31:0] instruction
