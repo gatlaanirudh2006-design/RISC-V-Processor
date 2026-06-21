6567524eb410fca458ce81a063257808a0d59495
gatlaanirudh2006 <gatlaanirudh2006@gmail.com>
2026-06-22 00:03:40 +0530

    Clean up comments in register_file.v
    Removed header comments from register_file.v

--- register_file.v
+++ register_file.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2026 11:41:02
-// Design Name: 
-// Module Name: register_file
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module register_file (
     input clk,
     input reset,
