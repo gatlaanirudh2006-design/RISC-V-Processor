7b03cbdc8173cbba77a95f3a368086bd57248f71
gatlaanirudh2006 <gatlaanirudh2006@gmail.com>
2026-06-22 00:04:11 +0530

    Update top_riscv.v

--- top_riscv.v
+++ top_riscv.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2026 11:48:20
-// Design Name: 
-// Module Name: top_riscv
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module top_riscv#(
 // ------------------------------------------------------------
