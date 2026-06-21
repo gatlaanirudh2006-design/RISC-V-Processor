f3f400f35763df0ec5f936c6e15bfe101f0dec3c
gatlaanirudh2006 <gatlaanirudh2006@gmail.com>
2026-06-22 00:03:19 +0530

    Clean up comments in pc.v
    Removed header comments from pc.v file.

--- pc.v
+++ pc.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.06.2026 11:36:45
-// Design Name: 
-// Module Name: pc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module pc (
     input clk,
